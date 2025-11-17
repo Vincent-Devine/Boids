6ba105a613b6782fff9763f21dbc217bfed5db4d
Vincent DEVINE <vincentdevine.pro@gmail.com>
2025-11-17 13:20:34 +0100

    UPDATE scene

--- Assets/Scripts/ObjectOriented/OO_Boid.cs
+++ Assets/Scripts/ObjectOriented/OO_Boid.cs
@@ -8,14 +8,14 @@ public class OO_Boid : MonoBehaviour
 
     private static readonly Vector3[] rayDirections2D =
     {
-        new Vector3( 1.0f, 0.0f,  0.0f),
-        new Vector3(-1.0f, 0.0f,  0.0f),
-        new Vector3( 0.0f, 0.0f,  1.0f),
-        new Vector3( 0.0f, 0.0f, -1.0f),
         new Vector3( 1.0f, 0.0f,  1.0f).normalized,
         new Vector3( 1.0f, 0.0f, -1.0f).normalized,
         new Vector3(-1.0f, 0.0f, -1.0f).normalized,
         new Vector3(-1.0f, 0.0f,  1.0f).normalized,
+        new Vector3( 1.0f, 0.0f,  0.0f),
+        new Vector3(-1.0f, 0.0f,  0.0f),
+        new Vector3( 0.0f, 0.0f,  1.0f),
+        new Vector3( 0.0f, 0.0f, -1.0f),
     };
     
     // State
